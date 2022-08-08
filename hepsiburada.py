eb72941ff06de8c0a6ac4ac0edc8dbc13e858747
Rewacode <105538090+rewaco@users.noreply.github.com>
2022-08-08 13:14:49 +0300

    Update hepsiburada.py

--- hepsiburada.py
+++ hepsiburada.py
@@ -1,85 +1,49 @@
-
 from bs4 import BeautifulSoup
 import requests
 
-
-
-
 headers= {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
-# bu kisim sizde calismazsa kendinize gore degisin
 
-class web(): # web isminde bir class olusturuyorum
-    urls = []
-    productInfo = []
+class web():
+    products = []
 
-    def __init__(self,product,page): # selfime product,page parametlerini ekliyorum
+    def __init__(self,product,page, filters):
         self.product = product
         self.page = page
+        self.filters = filters
 
-    def getProduct(self): # web classın altinda getProduct adli bir fonksiyon olusturdum
+    def getProduct(self):
         global headers
-        productPage = (f'{self.page}{self.product}')
-        head = requests.get(productPage, headers=headers) # olusturdugumuz headers burda kullaniyoruz
-        soup = BeautifulSoup(head.content, 'html.parser') # html.parse ile parse ediyoruz
+        productPage = f'{self.page}{self.product}&filtreler={self.filters}'
+        head = requests.get(productPage, headers=headers)
+        soup = BeautifulSoup(head.content, 'html.parser')
         products = soup.findAll("ul", {"class": "productListContent-wrapper"})
+
         for product in products:
-            for link in product.select(".productListContent-item a"): # https://www.hepsiburada.com/ara?q= linkinden sonra gelicek olan urun ismini ariyoruz
-                endpoint = link["href"]
-               
-                web.urls.append('https://www.hepsiburada.com' + endpoint) # burdada ekliyoruz urun ismini linkiminiz sonuna
-        
-        for i in web.urls: # bu bolumde aldıgımız linklerin icine girip fiyat rating ve isimlerini cekiyoruz
-            req = requests.get(i, headers=headers)
-            soup1 = BeautifulSoup(req.content, 'html.parser')
+            for aTag in product.select(".productListContent-item a"):
+                productSchema = {
+                    "name" : None,
+                    "price" : None,
+                    "rates" : [],
+                    "ratingCount" : "The product hasn't rating",
+                    "productImage" : None,
+                    "productURL" : None
+                }
+                print("----------------------------------------------------------------------------------------------")
+                productSchema["name"] = aTag.select("a div:last-child h3")[0].text
+                productSchema["price"] = aTag.select("a div:last-child div div[data-test-id='price-current-price']")[0].text
+                productSchema["productURL"] = f"https://www.hepsiburada.com/{aTag['href']}"
+                productSchema["productImage"] = aTag.select("img[src^='https://productimages']")[0]["src"]
+                if len(aTag.select("div[data-test-id = 'review'] div:last-child"))>0:
+                    productSchema["ratingCount"] = aTag.select("div[data-test-id = 'review'] div:last-child")[0].text
 
-            getName = soup1.find("h1", attrs={'class',"product-name best-price-trick"})
-            rating = soup1.find('span', attrs={'class', "rating-star"})
-            
-            getPrice = soup1.select('''span[data-bind="markupText:'currentPriceBeforePoint'"]''')
-            
-        
-            print(getName.text.strip())
-            print(getPrice[0].getText())
-            if(rating): # rating degeri none gelirse rating degeri yok yazmasini sagliyoruz
-                print(rating.text)
-            else:
-                print("Rating Yok")
+                for rate in aTag.select("a li[class^='moria-ProductCard'] div"):
+                    productSchema["rates"].append(rate["width"])
 
-            
-            
+                print(productSchema)
+                self.products.append(productSchema)
+                    
 
-        
-
-    
-    
-
-
-hepsiBurada = web(product='kale',page='https://www.hepsiburada.com/ara?q=') # son kisim da ise self olusturup product ve page parametlerine deger veriyoruz
-
-
-
-
-hepsiBurada.getProduct() # fonksiyonu cagırıyoruz
-
-
-
-
-
-
-        
-
-        
-
-        
-
-    
-    
-
-
-hepsiBurada = web(product='computer',page='https://www.hepsiburada.com/ara?q=')
-
-
-
+hepsiBurada = web(product='salıncak',page='https://www.hepsiburada.com/ara?q=', filters="fiyat:100-200")
 
 hepsiBurada.getProduct()
 
